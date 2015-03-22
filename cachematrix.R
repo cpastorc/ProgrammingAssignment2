99dd759f7a833447f2cf17eebeb7a1eab167359b
cpastorc <cpastor@nemsolutions.com>
2015-03-22 21:00:47 +0100

    week2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions check if the inverse of a matrix is available at the cache, and otherwise they calculate it 
 
-## Write a short comment describing this function
+## This function creates a list with functions to manage the value of the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function (inv) i <<- inv
+  getinv <- function () i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of a matrix, but checks first if it is available at the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if (!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
